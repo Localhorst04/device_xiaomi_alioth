19147f01fcf13a8e22a54a983c45fa167d768e04
Localhorst04 <aaron.laib@protonmail.com>
2025-06-25 23:50:50 +0000

    alioth: voltageOS bringup adapt LineageOS entries to VoltageOS drop AxoinOS entries as they will not be needed anymore comment out Leica for now

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ TARGET_IS_VAB := true
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
 # Miuicamera
-$(call inherit-product, vendor/xiaomi/camera/miuicamera.mk)
+# $(call inherit-product, vendor/xiaomi/camera/miuicamera.mk)
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
@@ -34,13 +34,13 @@ PRODUCT_PACKAGES += \
     libpiex_shim
 
 # Miui Camera STLicense
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
+# PRODUCT_COPY_FILES += \
+    # $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_PACKAGES += \
     AliothFrameworks \
--- voltage_alioth.mk
+++ voltage_alioth.mk
@@ -9,28 +9,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit Camera-related flags
-TARGET_USES_MIUI_CAMERA := true
-TARGET_INCLUDES_MIUI_CAMERA := true
+TARGET_USES_MIUI_CAMERA := false
+TARGET_INCLUDES_MIUI_CAMERA := false
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := voltage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-
-AXION_CAMERA_REAR_INFO := 48,8,5
-AXION_CAMERA_FRONT_INFO := 20
-AXION_MAINTAINER := ꜱᴀɴᴊɪ_|_サンジ
-AXION_PROCESSOR := Snapdragon_870
-# Define small and big core groups
-AXION_CPU_SMALL_CORES := 0,1,2,3
-AXION_CPU_BIG_CORES := 4,5,6,7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
